85402e9fb377cee059e1d2beaf90f10aa69e2504
Mauro Pellonara <59605693+MauroPello@users.noreply.github.com>
2020-11-09 18:40:21 +0100

    Update marginal_profit.py

--- marginal-profits/marginal_profit.py
+++ marginal-profits/marginal_profit.py
@@ -13,14 +13,14 @@ def main():
     # the user types the constant (ex. 7)
     while True:
         try:
-            tmp = int(input("Inserire costante Ricavo/Quantità: "))
+            tmp = int(input("Enter Costs/Proceeds constant: "))
             if tmp <= 0:
                 raise ValueError
             else:
                 constant = (tmp, )
                 break
         except ValueError:
-            print("La costante deve essere un numero positivo!")
+            print("The constant has to be a positive number!")
 
     # reads the production costs for n products manufactured starting from 0
     costs = []
@@ -47,28 +47,28 @@ def main():
     # calculates whether or not the company is doing good and profiting from it's products
     outcome = []
     for item in marginal_profits:
-        outcome.append("Profitto" if item > 0 else "Non Profitto")
+        outcome.append("Profit" if item > 0 else "No Profit")
 
     # assembling all the data gathered
     table_data = {
-        "Quantità": [item for item in range(quantity)],
-        "Ricavi": proceeds,
-        "Costi": costs,
-        "Profitti": profits,
-        "ΔRicavi": marginal_proceeds,
-        "ΔCosti": marginal_costs,
-        "ΔProfitti": marginal_profits,
-        "Conclusione": outcome
+        "Quantity": [item for item in range(quantity)],
+        "Proceeds": proceeds,
+        "Costs": costs,
+        "Profits": profits,
+        "ΔProceeds": marginal_proceeds,
+        "ΔCosts": marginal_costs,
+        "ΔProfits": marginal_profits,
+        "Outcome": outcome
     }
 
-    table = OnlyColumnsTable(f"Tabella dei Margini (k = {constant[0]})", 20)    # creates a new table
+    table = OnlyColumnsTable(f"Profits Table (k = {constant[0]})", 20)    # creates a new table
     table.fill(table_data)  # fills the newly created table
     table.format(15, 15)    # formats the table
     table.save_to_file("table", "png")   # saves the table to .png
 
-    graph = MultipleLinesGraph(f"Costi/Ricavi Azienda (k = {constant[0]})", "Quantità", "Prezzi", 15, 15, 20)     # creates a new graph
-    graph.add_line(range(quantity), costs, "Costi")     # adding the costs line
-    graph.add_line(range(quantity), proceeds, "Ricavi")     # adding the proceeds line
+    graph = MultipleLinesGraph(f"Costs/Proceeds (k = {constant[0]})", "Quantity", "Prices", 15, 15, 20)     # creates a new graph
+    graph.add_line(range(quantity), costs, "Costs")     # adding the costs line
+    graph.add_line(range(quantity), proceeds, "Proceeds")     # adding the proceeds line
     graph.set_legend("lower right", 20)         # setting up the legend
     graph.show_values_in_x_axis(range(quantity))        # enables better viewing of quantity values
     graph.show_values_in_y_axis(costs)      # enables better viewing of costs values
