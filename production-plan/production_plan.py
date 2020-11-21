d738da25c9a0dde6708f20de77e501b3eb9dc890
MauroPello <mauro.pellonara@outlook.com>
2020-11-21 13:24:20 +0100

    Changed Module Names

--- production-plan/production_plan.py
+++ production-plan/production_plan.py
@@ -4,7 +4,7 @@ import inspect
 PACKAGE_PARENT = '..'
 SCRIPT_DIR = os.path.realpath(os.path.dirname(inspect.getfile(inspect.currentframe())))
 sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
-from data_plotting_lib.simple_data_plotting_lib import OnlyColumnsTable, MultipleLinesGraph
+from data-plotting-lib.simple_data_plotting_lib import OnlyColumnsTable, MultipleLinesGraph
 from datetime import datetime,  timedelta
 
 
